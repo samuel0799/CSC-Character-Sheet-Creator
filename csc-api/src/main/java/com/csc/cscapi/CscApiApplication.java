d94b8a2ade6b06c139c1ff4dc2c9f8aaae6f986b
samuel vitor <samuelgaldinno@hotmail.com>
2023-03-30 19:07:13 -0300

    primeiro commit

---
+++ csc-api/src/main/java/com/csc/cscapi/CscApiApplication.java
@@ -0,0 +1,13 @@
+package com.csc.cscapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CscApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CscApiApplication.class, args);
+	}
+
+}
---
+++ csc-api/src/test/java/com/csc/cscapi/CscApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc.cscapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CscApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
